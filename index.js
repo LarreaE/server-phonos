2453c76139c52eb78c9e9c92012987e93eca23a4
LarreaE <eneko.larrea@ikasle.aeg.eus>
2024-05-22 18:34:50 +0100

    fixed del

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ app.post('/reg', async(req,res) => {
 });
 app.post('/del', async(req,res) => {
 
-    const nameReg = req.body.name
+    const nameReg = req.body.username
     const query = "delete from users where username = '" + nameReg + "')"
     console.log(query);
     db.query(
